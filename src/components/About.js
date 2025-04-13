3c51ab7dcaedf88138d24068de06ad007964f4b0
TheCoderBuilder-dev <brianinesh@gmail.com>
2025-04-13 16:29:49 +0300

    kjj

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { image } from "../data/data";
+// src/components/About.js
+
+import React from 'react';
+import { image } from '../data/user';  // Importing image variable from user.js
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is some content about me.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
---
+++ src/data/user.js
@@ -0,0 +1,5 @@
+// src/data/user.js
+
+export const name = "Liza";
+export const city = "New York";
+export const image = "https://i.imgur.com/mV8PQxj.gif";
